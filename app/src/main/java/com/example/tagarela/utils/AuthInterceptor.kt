a693f0b87d71da4d80b167e8c9c94f109e47807d
Murilo <127051508+murilo587@users.noreply.github.com>
2025-04-11 01:05:07 -0300

    feat: add AuthInterceptor for request authorization

---
+++ app/src/main/java/com/example/tagarela/utils/AuthInterceptor.kt
@@ -0,0 +1,15 @@
+package com.example.tagarela.utils
+
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+class AuthInterceptor(private val token: String) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest: Request = chain.request()
+        val newRequest = originalRequest.newBuilder()
+            .addHeader("Authorization", "Bearer $token")
+            .build()
+        return chain.proceed(newRequest)
+    }
+}
